35812ea94d332487c3aafcc601fe672f0da36ba1
Saurav Kumar <2130171@kiit.ac.in>
2023-02-04 19:21:03 +0530

    added a schema for candidate

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const candidate = require('./routes/candidate');
 require('dotenv').config();
 
 
-const app = express();
-
+const DB_URI = process.env.DB_URI;
 const PORT = process.env.PORT;
 
-app.get((req, res) => {
+const app = express();
+
+mongoose.set('strictQuery', false);
+mongoose.connect(DB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(app.listen(PORT, () => {
+    console.log(`Listening on Port ${PORT}`);
+}))
+
+
+
+app.get('/',(req, res) => {
     console.log(req.body);
 })
 
-app.listen(PORT, function () {
-    console.log(`Program is running on: ${PORT}`);
-});
+app.use('/candidate',candidate);
